aadd41b181ac007a5255e9f3ad667bc82414ea45
Spencer Krum <nibz@spencerkrum.com>
2021-01-31 11:07:52 -0600

    Update runbot script

--- run_bot.sh
+++ run_bot.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+docker pull quay.io/nibalizer/discordstonksbot
+docker stop discordstonk
+docker rm discordstonk
+
 docker run -d \
   -it \
   --env-file vals.txt \
   --restart=always \
   --name discordstonk \
-  quay.io/nibalizer/discordstonksbot
+  quay.io/nibalizer/discordstonksbot:latest
